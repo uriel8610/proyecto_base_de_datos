be0d71b4cf42d5f2619f9681837956ecb0a3bfd9
YAGAMIOO7 <julio_dante007@hotmail.com>
2014-03-28 11:19:11 -0600

    Script de las Vistas

---
+++ vistas.sql
@@ -0,0 +1,20 @@
+﻿USE sysmed;
+
+DROP VIEW IF EXISTS vista_medico;
+DROP VIEW IF EXISTS vista_usuario;
+DROP VIEW IF EXISTS vista_paciente;
+
+CREATE VIEW sysmed.vista_medico AS
+SELECT Persona.Nombre, Persona.Apellido, Especialidad.Descripcion
+FROM Persona, Medico, Especialidad
+WHERE Persona.idPersona = Medico.idMedico AND Especialidad.idEspecialidad = Medico.idEspecialidad;
+
+CREATE VIEW sysmed.vista_paciente AS
+SELECT Persona.Nombre, Persona.Apellido, Persona.idPersona, Paciente.idPaciente
+FROM Persona, Paciente
+WHERE Persona.idPersona = Paciente.idPaciente;
+
+CREATE VIEW sysmed.vista_usuario AS
+SELECT Persona.Nombre, Persona.Apellido
+FROM Persona, Usuario
+WHERE Persona.idPersona = Usuario.idUsuario;
