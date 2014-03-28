722b953e81c1e55eeafebada5266dc82ce111a4d
YAGAMIOO7 <julio_dante007@hotmail.com>
2014-03-28 12:19:51 -0600

    Completar Horarios, agregar mas personas, usuarios y pacientes

--- desarrollo.sql
+++ desarrollo.sql
@@ -93,6 +93,13 @@ INSERT INTO tipoUsuario(Descripcion)VALUES('Usuario');
 INSERT INTO Horario(idHorario, Descripcion)VALUES(0001, '8:00 am - 9:00 am');
 INSERT INTO Horario(Descripcion)VALUES('9:00 am - 10:00 am');
 INSERT INTO Horario(Descripcion)VALUES('10:00 am - 11:00 am');
+INSERT INTO Horario(Descripcion)VALUES('11:00 am - 12:00 pm');
+INSERT INTO Horario(Descripcion)VALUES('12:00 am - 01:00 pm');
+INSERT INTO Horario(Descripcion)VALUES('01:00 pm - 02:00 pm');
+INSERT INTO Horario(Descripcion)VALUES('02:00 pm - 03:00 pm');
+INSERT INTO Horario(Descripcion)VALUES('03:00 am - 04:00 pm');
+INSERT INTO Horario(Descripcion)VALUES('04:00 pm - 05:00 pm');
+INSERT INTO Horario(Descripcion)VALUES('05:00 pm - 06:00 pm');
 
 INSERT INTO Especialidad(idEspecialidad, Descripcion)VALUES(0001, 'Medico General');
 INSERT INTO Especialidad(Descripcion)VALUES('Medico Cirujano');
@@ -115,26 +122,40 @@ VALUES('Yadira','Morales','Juarez #9','123456789','jady@',27,'F','15/08/2010');
 INSERT INTO Persona(Nombre,Apellido,Direccion,Telefono,eMail,Edad,Sexo,FechaRegistro)
 VALUES('Juan Ramon','Calvillo','Juarez #10','123456789','juanR@',20,'M','01/06/2012');
 
+INSERT INTO Persona(Nombre,Apellido,Direccion,Telefono,eMail,Edad,Sexo,FechaRegistro)
+VALUES('Edna Yuritzi','Sanches','Main #13','987654321','edn@',22,'F','01/07/2012');
+INSERT INTO Persona(Nombre,Apellido,Direccion,Telefono,eMail,Edad,Sexo,FechaRegistro)
+VALUES('Monserrat','Martinez','Juriquilas #15','98748192','monse@',25,'F','08/07/2013');
+INSERT INTO Persona(Nombre,Apellido,Direccion,Telefono,eMail,Edad,Sexo,FechaRegistro)
+VALUES('Alexandre','Zavala','Hidalgo #18','74189536','alex@',27,'M','01/08/2013');
+INSERT INTO Persona(Nombre,Apellido,Direccion,Telefono,eMail,Edad,Sexo,FechaRegistro)
+VALUES('Pablo Jesus','Suarez','Pavon #10','21479584','pablo@',29,'M','01/05/2013');
+
 INSERT INTO Medico(idMedico,idPersona,idEspecialidad,Cedula,Estado)VALUES(0001,0001,0001,'MANU0002','Alta');
 INSERT INTO Medico(idPersona,idEspecialidad,Cedula,Estado)VALUES(0003,0002,'AMAU0002','Alta');
 INSERT INTO Medico(idPersona,idEspecialidad,Cedula,Estado) VALUES (0005,0003,'FERU0002','Alta');
 INSERT INTO Medico(idPersona,idEspecialidad,Cedula,Estado) VALUES (0007,0001,'YADU0002','Baja');
 
-INSERT INTO Usuario(idUsuario,idPersona,idTipoUsuario,Contrasena,Estado) 
+INSERT INTO Usuario(idUsuario,idPersona,idTipoUsuario,Contrasena,Estado)
 VALUES (0001,0002,0001,'jose','Alta');
-INSERT INTO Usuario(idPersona,idTipoUsuario,Contrasena,Estado) 
+INSERT INTO Usuario(idPersona,idTipoUsuario,Contrasena,Estado)
 VALUES (0004,0002,'ana','Alta');
+INSERT INTO Usuario(idPersona,idTipoUsuario,Contrasena,Estado)
+VALUES (0010,0002,'monse','Alta');
 
 INSERT INTO Paciente(idPaciente,idPersona) VALUES (0001,0006);
 INSERT INTO Paciente(idPersona) VALUES (0008);
+INSERT INTO Paciente(idPersona) VALUES (0009);
+INSERT INTO Paciente(idPersona) VALUES (0011);
+INSERT INTO Paciente(idPersona) VALUES (0012);
 
 INSERT INTO Cita(idCita, idMedico, idPaciente, idHorario, idUsuario, Fecha, FechaActual, HoraActual, Estado)
 VALUES(0001, 0001, 0001, 0001, 0001, '17/03/2014','15/03/2014', '10:00', 'realizada');
 INSERT INTO Cita(idMedico, idPaciente, idHorario, idUsuario, Fecha, FechaActual, HoraActual, Estado)
-VALUES(0001, 0002, 0003, 0001, '20/03/2014','18/03/2014', '13:00', 'pendiente');
+VALUES(0004, 0009, 0003, 0001, '20/03/2014','18/03/2014', '13:00', 'pendiente');
 INSERT INTO Cita(idMedico, idPaciente, idHorario, idUsuario, Fecha, FechaActual, HoraActual, Estado)
-VALUES(0002, 0002, 0002, 0002, '20/03/2014','18/03/2014', '12:00', 'pendiente');
+VALUES(0003, 0011, 0004, 0002, '20/03/2014','18/03/2014', '12:00', 'pendiente');
 INSERT INTO Cita(idMedico, idPaciente, idHorario, idUsuario, Fecha, FechaActual, HoraActual, Estado)
-VALUES(0002, 0001, 0001, 0002, '16/01/2014','15/03/2014', '15:00', 'realizada');
+VALUES(0002, 0012, 0007, 0002, '16/01/2014','15/03/2014', '15:00', 'realizada');
 INSERT INTO Cita(idMedico, idPaciente, idHorario, idUsuario, Fecha, FechaActual, HoraActual, Estado)
-VALUES(0001, 0001, 0001, 0001, '15/01/2014','14/03/2014', '06:00', 'cancelada');
+VALUES(0001, 0002, 0006, 0003, '15/01/2014','14/03/2014', '06:00', 'cancelada');
