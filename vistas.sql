06955a9d13c3499250361c74bb810958a8f8a706
YAGAMIOO7 <julio_dante007@hotmail.com>
2014-03-28 12:16:08 -0600

    Modificacion de una vista

--- vistas.sql
+++ vistas.sql
@@ -5,16 +5,16 @@ DROP VIEW IF EXISTS vista_usuario;
 DROP VIEW IF EXISTS vista_paciente;
 
 CREATE VIEW sysmed.vista_medico AS
-SELECT Persona.Nombre, Persona.Apellido, Especialidad.Descripcion
+SELECT Persona.Nombre, Persona.Apellido, Especialidad.Descripcion, Persona.idPersona, Medico.idMedico
 FROM Persona, Medico, Especialidad
 WHERE Persona.idPersona = Medico.idMedico AND Especialidad.idEspecialidad = Medico.idEspecialidad;
 
 CREATE VIEW sysmed.vista_paciente AS
-SELECT Persona.Nombre, Persona.Apellido
+SELECT Persona.Nombre, Persona.Apellido, Persona.idPersona, Paciente.idPaciente
 FROM Persona, Paciente
 WHERE Persona.idPersona = Paciente.idPaciente;
 
 CREATE VIEW sysmed.vista_usuario AS
-SELECT Persona.Nombre, Persona.Apellido
+SELECT Persona.Nombre, Persona.Apellido, Persona.idPersona, Usuario.idUsuario
 FROM Persona, Usuario
 WHERE Persona.idPersona = Usuario.idUsuario;
